9492261e44414c036f53b9358f674569c7c9f4a7
Ömer Efe ÇELİK <omerefecelik3@gmail.com>
2022-10-10 21:42:07 +0300

    as

--- main.js
+++ main.js
@@ -69,15 +69,16 @@ class TableCsv {
 }
 
 const tableRoot = document.querySelector("#csvRoot");
-const csvFileInput = document.querySelector("#csvFileInput");
+//const csvFileInput = document.querySelector("#csvFileInput");
 const tableCsv = new TableCsv(tableRoot);
 
-csvFileInput.addEventListener("change", (e) => {
-  Papa.parse(csvFileInput.files[0], {
-    delimiter: ",",
-    skipEmptyLines: true,
-    complete: (results) => {
-      tableCsv.update(results.data.slice(1), results.data[0]);
-    }
-  });
+
+
+Papa.parse("./list.csv", {
+  delimiter: ",",
+  download: true,
+  skipEmptyLines: true,
+  complete: (results) => {
+    tableCsv.update(results.data.slice(1), results.data[0]);
+  }
 });
